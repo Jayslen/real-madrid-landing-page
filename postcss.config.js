dd126f2709c6832425ad1f555ce840d49f43a6ac
jayslen <jayslen134@gmail.com>
2024-01-28 23:12:38 -0400

    rss feed added

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,8 @@
-module.exports = {
-    plugins: {
-      'postcss-import': {},
-      'tailwindcss/nesting': {},
-      tailwindcss: {},
-      autoprefixer: {},
-    }
+export default {
+  plugins: {
+    'postcss-import': {},
+    'tailwindcss/nesting': {},
+    tailwindcss: {},
+    autoprefixer: {},
   }
+};
